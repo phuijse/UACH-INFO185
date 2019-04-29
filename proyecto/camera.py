d1938b0c731cec8ae065e95a8b35e545d4076854
phuijse <pablo.huijse@gmail.com>
2019-04-29 11:59:42 -0400

    código trabajo práctico

---
+++ proyecto/camera.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import PIL.Image
+import io
+#import ipywidgets as widgets
+
+def display_frame(out_widget, frame):
+    f = io.BytesIO() 
+    PIL.Image.fromarray(frame).save(f, 'JPEG')
+    out_widget.value = f.getvalue()
+    
+    
+class CameraReader:
+    
+    def __init__(self, spthreshold=0.999):
+        print("Iniciando lector de camara")
+        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+        print("OpenCV version: major: {0}, minor: {1}, subminor: {2}".format(major_ver, minor_ver, subminor_ver))
+        videopath = 'torres_paine_pan.mp4'
+        self.vid = cv2.VideoCapture(videopath)
+        self.properties = {'width': int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                           'height': int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+                           'fps': self.vid.get(cv2.CAP_PROP_FPS)}
+        
+        spprob = np.random.rand(self.properties['height'], self.properties['width'])
+        self.smask = spprob > spthreshold
+        self.pmask = spprob < 1. - spthreshold
+        self.frame_number = 0
+        y = np.linspace(0, self.properties['height'], num=self.properties['height'])
+        x = np.linspace(0, self.properties['width'], self.properties['width'])
+        X, Y = np.meshgrid(x, y)
+        self.noise_angle = 2.0*np.pi*(20*Y + 2*X)
+        assert self.vid.isOpened(), "Video no encontrado" 
+        
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """
+        Función iteradora, cada vez que es llamada lee y retorna un frame de video
+        
+        Termina cuando el video ha sido consumido por completo
+        """
+        successfully_read, frame = self.vid.read()
+        if not successfully_read:
+            raise StopIteration
+        
+        self.current_original_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        noisy_frame = self.current_original_frame.copy().astype(np.float32)/255.        
+        periodic_noise = 0.5 + 0.5*np.cos(self.noise_angle  - self.frame_number*0.1)
+        periodic_noise = periodic_noise.reshape(noisy_frame.shape)
+        noisy_frame += periodic_noise
+        noisy_frame /= np.amax(np.abs(noisy_frame))
+        #noisy_frame[self.smask] = 1.
+        #noisy_frame[self.pmask] = 0.
+        self.frame_number += 1
+        return (255*noisy_frame).astype(np.uint8)
+    
+    def get_resolution(self):
+        """
+        Retorna una tupla con el alto (filas) y ancho (columnas) del video
+        """
+        return (self.properties['height'], self.properties['width'])
+    
+    def get_fps(self):
+        """
+        Retorna los cuadros por segundo (fps) del video
+        """
+        return self.properties['fps']
+    
+    def error(self, recovered_frame):
+        """
+        Retorna el error  cuadrático medio entre el frame original y el frame recuperado
+        """
+        return np.average((recovered_frame - self.current_original_frame)**2)
+        
+    def __del__(self):
+        self.vid.release()  
+        cv2.destroyAllWindows()
+        
+"""     
+class VideoPlayer:
+    
+    def __init__(self, width, height, fps):
+        self.out = widgets.Image(layout=widgets.Layout(height=str(height), width=str(width)))
+        
+    def push_frame(self, frame):
+        f = BytesIO() 
+        PIL.Image.fromarray(frame).save(f, 'JPEG')
+        self.out.value = f.getvalue()
+        cv2.waitKey(10)
+"""   
